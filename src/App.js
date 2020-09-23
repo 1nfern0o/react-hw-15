0c952d5f99582e87cb479da84be1480b12bf12b9
Roman <romantcvetkov@gmail.com>
2020-09-24 01:08:27 +0300

    add react-hw-15

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Posts from "./Components/Post/post";
+import vader from "./vader.jpg";
+import commVader from "./comm_vader.jpg";
+
+const ANAKIN_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Anakin-Jedi.jpg/220px-Anakin-Jedi.jpg";
+
+const RAY_IMAGE = "https://specials-images.forbesimg.com/imageserve/5e63b3c8e1e617000759130e/960x0.jpg?fit=scale";
+
+
+/*const POST_LIST = [
+  {
+    author: {
+      name: "Anakin skywalker",
+      photo: ANAKIN_IMAGE,
+      nickname: "@dart_vader"
+    },
+    content: "WTF? Who is Ray? Why she is Skywalker? Luke...?",
+    image: {RAY_IMAGE},
+    date: "26 февр."
+  }
+];*/
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Posts author={{
+          name: "Anakin skywalker",
+          photo: ANAKIN_IMAGE,
+          nickname: "@dart_vader"
+        }}
+               content="WTF? Who is Ray? Why she is Skywalker? Luke...?"
+               image={RAY_IMAGE}
+               date={"26 февр."}
+        />
+          <Posts author={{
+              name: "Twin Anakin skywalker",
+              photo: vader,
+              nickname: "@dart_vader"
+          }}
+                 content="Darth Vader is a fictional character in the Star Wars franchise. Born Anakin Skywalker"
+                 image={commVader}
+                 date={"20 янв."}
+          />
+        {/* { POST_LIST.map(post => <Posts {...post}/>)}*/}
+      </div>
   );
 }
 
