d6f413499da1bd70d1cd5795b3bc885f24364d4e
Roman <romantcvetkov@gmail.com>
2020-09-24 01:08:57 +0300

    Updates

---
+++ precache-manifest.f0c5ffee1a4cee5051d77cf184edd462.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74d2bcec713a2cc29606d23e13d739d",
+    "url": "/react-hw-15/index.html"
+  },
+  {
+    "revision": "b1cf8afea5f524ad7359",
+    "url": "/react-hw-15/static/css/main.d36685a5.chunk.css"
+  },
+  {
+    "revision": "44a4140df0b01f538fae",
+    "url": "/react-hw-15/static/js/2.160d6dbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-15/static/js/2.160d6dbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1cf8afea5f524ad7359",
+    "url": "/react-hw-15/static/js/main.ebe7a9eb.chunk.js"
+  },
+  {
+    "revision": "6741cc4b8375ec6bbc84",
+    "url": "/react-hw-15/static/js/runtime-main.3d491542.js"
+  },
+  {
+    "revision": "5cf6a97acc9ebab948aab4fbf71e437e",
+    "url": "/react-hw-15/static/media/comm_vader.5cf6a97a.jpg"
+  },
+  {
+    "revision": "b89805e1fb91d845bc54ea83226544a8",
+    "url": "/react-hw-15/static/media/comments.b89805e1.svg"
+  },
+  {
+    "revision": "adfa5cd23bb7c01352a586b73f5b44e6",
+    "url": "/react-hw-15/static/media/comments_hover.adfa5cd2.svg"
+  },
+  {
+    "revision": "7478f0159c9ebdb3fe4a08c966f82617",
+    "url": "/react-hw-15/static/media/down-arrow.7478f015.svg"
+  },
+  {
+    "revision": "a8cd76c1934dc020e510f93a33774305",
+    "url": "/react-hw-15/static/media/heart.a8cd76c1.svg"
+  },
+  {
+    "revision": "7746f5e4d82a2cf71a3d1f30fdef6ace",
+    "url": "/react-hw-15/static/media/like.7746f5e4.svg"
+  },
+  {
+    "revision": "8b6117db18ab7a404bda261f08ab8dc1",
+    "url": "/react-hw-15/static/media/send.8b6117db.svg"
+  },
+  {
+    "revision": "f582311e8d988d28f0a51efdbdc79163",
+    "url": "/react-hw-15/static/media/send_hover.f582311e.svg"
+  },
+  {
+    "revision": "bd40ebf4cb91f5dd1ce579ad951632eb",
+    "url": "/react-hw-15/static/media/share.bd40ebf4.svg"
+  },
+  {
+    "revision": "a8433b84baf7443ed6907b0501669c52",
+    "url": "/react-hw-15/static/media/share_hover.a8433b84.svg"
+  },
+  {
+    "revision": "2182b8124b9b0ea562b9a696eb0260c9",
+    "url": "/react-hw-15/static/media/vader.2182b812.jpg"
+  },
+  {
+    "revision": "c9a1728db4a67a936a2c67c3a7e5a99f",
+    "url": "/react-hw-15/static/media/verified.c9a1728d.svg"
+  }
+]);
